6ad8936433862a074d0f0f287ab8a2e00caefe1b
Asitha Navaratne <asithan476@gmail.com>
2021-09-29 20:26:06 +0530

    Justified comments

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(void){
 	PORT_INIT();
 	SPI_MASTER_INIT();
 	
-	char *transmit = "Hello there!";			///< Data to be sent.
+	char *transmit = "Hello there!";				///< Data to be sent.
 	
 	SPI_SEND_STRING(transmit);
 }
@@ -22,9 +22,9 @@ int main(void){
  */
 
 void PORT_INIT(void){
-	DDRB |= (1<<PB4)|(1<<PB5)|(1<<PB7);			///< Set MOSI, SCK and SS pins to output.
-	DDRB &= ~(1<<PB6);					///< Set MISO pin to input.
-	PORTB |= (1<<PB4);					///< Set SS pin logic HIGH before beginning to transmit data.
+	DDRB |= (1<<PB4)|(1<<PB5)|(1<<PB7);				///< Set MOSI, SCK and SS pins to output.
+	DDRB &= ~(1<<PB6);						///< Set MISO pin to input.
+	PORTB |= (1<<PB4);						///< Set SS pin logic HIGH before beginning to transmit data.
 }
 
 /*!
@@ -32,7 +32,7 @@ void PORT_INIT(void){
  */
 
 void SPI_MASTER_INIT(void){
-	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);			///< Enable SPI, select Master SPI mode and set SCK frequency to Fosc/16.
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);				///< Enable SPI, select Master SPI mode and set SCK frequency to Fosc/16.
 }
 
 /*!
@@ -41,11 +41,11 @@ void SPI_MASTER_INIT(void){
  */
 
 void SPI_SEND_STRING(char *data){
-	PORTB &= ~(1<<PB4);					///< Set SS pin LOW to wake up slave device.
+	PORTB &= ~(1<<PB4);						///< Set SS pin LOW to wake up slave device.
 	for(uint8_t i=0;data[i]!=0;i++){
-		SPDR = data[i];					///< Load the data byte to the SPDR shift register to transmit.
-		while(!(SPSR & (1<<SPIF)));			///< Wait until the full data byte is received.
-		PORTA = SPDR;					///< Display ACK byte received from slave.
+		SPDR = data[i];						///< Load the data byte to the SPDR shift register to transmit.
+		while(!(SPSR & (1<<SPIF)));				///< Wait until the full data byte is received.
+		PORTA = SPDR;						///< Display ACK byte received from slave.
 	}
 	PORTB |= (1<<PB4);
 }
